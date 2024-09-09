527ca4407d1a2f2872f140b57c570f220bc617bf
Алексей Пожар <a.pozhar@x5.ru>
2024-09-09 16:02:08 +0300

    Update static

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 app.include_router(main.router)
 app.include_router(front.router)
 
-app.mount('/static/pastebeen', StaticFiles(directory="static/pastebeen"), 'static_pastebeen')
+app.mount('/static/pastebeen', StaticFiles(directory="pastebeen/static"), 'pastebeen_static')
 
 
